db1db88000f64ed5b55fb01bfc1aa8840dd7017a
NikitaKorzh <nik.korzh.ita@gmail.com>
2022-10-17 14:57:50 +0300

    deleteOneQuantity added

--- src/main/java/ua/softserve/dao/impl/QuantityDaoImpl.java
+++ src/main/java/ua/softserve/dao/impl/QuantityDaoImpl.java
@@ -1,14 +1,19 @@
 package ua.softserve.dao.impl;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.jdbc.object.SqlQuery;
-import org.springframework.transaction.annotation.Transactional;
+import org.hibernate.query.Query;
 import ua.softserve.config.HibernateConfig;
 import ua.softserve.dao.QuantityDao;
 import ua.softserve.model.Book;
 import ua.softserve.model.Quantity;
 import ua.softserve.model.enums.Type;
+
+import java.util.List;
+
 
 public class QuantityDaoImpl implements QuantityDao {
 
@@ -25,11 +30,23 @@ public class QuantityDaoImpl implements QuantityDao {
             for (int i = 0; i < count; i++) {
                 session.persist(new Quantity(book, Type.FREE));
             }
+            session.getTransaction().commit();
         }
     }
 
     @Override
     public void deleteOneQuantity(long id) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Quantity> cr = cb.createQuery(Quantity.class);
+            Root<Quantity> root = cr.from(Quantity.class);
+            cr.select(root);
 
+            Query<Quantity> query = session.createQuery(cr);
+            List<Quantity> results = query.getResultList();
+            session.remove(results.get(0));
+            session.getTransaction().commit();
+        }
     }
 }
